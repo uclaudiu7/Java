7c803926b2cf6e284d97bb75cd610ad18255a2e5
Claudiu Ursescu <ursescuclaudiu7@gmail.com>
2023-02-28 17:04:45 +0200

    test commit

--- src/week2/Main.java
+++ src/week2/Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Location burla = new Location("Burla", locationType.VILLAGE);
         Location iasi = new Location("Iasi", locationType.CITY);
+        Location gatwick = new Location("Gatwick Airport", locationType.AIRPORT);
         Road dn1 = new Road(roadType.HIGHWAY, 220, 180);
         Road m1 = new Road(roadType.EXPRESS, 4000, 200);
         System.out.println(burla);
         System.out.println(iasi);
+        System.out.println(gatwick);
         System.out.println(dn1);
         System.out.println(m1);
     }
