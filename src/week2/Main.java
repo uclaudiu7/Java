1b79829448245c4b7d3000d248219ae0aa928dd9
Claudiu Ursescu <ursescuclaudiu7@gmail.com>
2023-02-28 22:22:26 +0200

    test 2

--- src/week2/Main.java
+++ src/week2/Main.java
@@ -7,10 +7,12 @@ public class Main {
         Location gatwick = new Location("Gatwick Airport", locationType.AIRPORT);
         Road dn1 = new Road(roadType.HIGHWAY, 220, 180);
         Road m1 = new Road(roadType.EXPRESS, 4000, 200);
+        Road dn67 = new Road(roadType.HIGHWAY, 100, 100);
         System.out.println(burla);
         System.out.println(iasi);
         System.out.println(gatwick);
         System.out.println(dn1);
         System.out.println(m1);
+        System.out.println(dn67);
     }
 }
