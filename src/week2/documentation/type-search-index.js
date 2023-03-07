61af6c3a17d6eccbde66b55c4ab8dbaebd55ee19
Claudiu Ursescu <ursescuclaudiu7@gmail.com>
2023-03-07 17:40:40 +0200

    javadoc for week2

---
+++ src/week2/documentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"week2","l":"Airport"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"week2","l":"City"},{"p":"week2","l":"Compulsory"},{"p":"week2","l":"GasStation"},{"p":"week2","l":"Homework"},{"p":"week2","l":"Location"},{"p":"week2","l":"Road"}];updateSearchResults();
